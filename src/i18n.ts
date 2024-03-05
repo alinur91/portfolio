6d9a98bb0d3e503a47e116b3571b76068fe854aa
alisher <areshil91@gmail.com>
2024-03-05 20:13:36 +0600

    reversal

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,6 +5,6 @@ import Backend from "i18next-http-backend";
 
 i18next.use(LanguageDetector).use(initReactI18next).use(Backend).init({
   debug: true,
-  fallbackLng: "en-US",
+  fallbackLng: "en",
   returnObjects: true,
 });
