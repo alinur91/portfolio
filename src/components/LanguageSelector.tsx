6c930680bc9b4aea60d75a0c7a8da28b609670e6
alisher <areshil91@gmail.com>
2024-03-05 16:08:54 +0600

    refactoring in languageSelector.tsx

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -2,54 +2,17 @@ import { useState } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import { useTranslation } from "react-i18next";
-import { LanguagesType } from "../ts/types";
 
 const LanguageSelector = () => {
   const [showLanguageOptions, setShowLanguageOptions] = useState(false);
   const { i18n } = useTranslation();
-
   const languages = [
     { code: "en", language: "English" },
     { code: "ru", language: "Русский" },
   ];
 
-  const getShorthandLanguageName = (
-    languages: LanguagesType[],
-    i18Language: string
-  ) => {
-    const language = languages.filter((lan) => lan.code === i18Language)[0]
-      .language;
-
-    return (
-      <span> {language.charAt(0).toUpperCase() + language.slice(1, 3)}</span>
-    );
-  };
-
-  const showListOfLanguages = (showLanguageOptions: boolean) => {
-    return showLanguageOptions ? (
-      <ul className="absolute top-[100%] flex flex-col gap-4 w-full">
-        {languages.map((languageInfo) => (
-          <li
-            className={`${
-              languageInfo.code === i18n.language ? "text-slate-400" : ""
-            }`}
-            key={languageInfo.code}
-            onClick={() => i18n.changeLanguage(languageInfo.code)}
-          >
-            {languageInfo.language}
-          </li>
-        ))}
-      </ul>
-    ) : undefined;
-  };
-
-  const showArrowIcon = (showLanguageOptions: boolean) => {
-    return (
-      <div className="group-hover:text-slate-400">
-        {showLanguageOptions ? <IoIosArrowUp /> : <IoIosArrowDown />}
-      </div>
-    );
-  };
+  const language = languages.filter((lan) => lan.code === i18n.language)[0]
+    .language;
 
   return (
     <div
@@ -57,10 +20,26 @@ const LanguageSelector = () => {
       className="relative  gap-1 cursor-pointer  text-xl sm:text-2xl bg-slate-100  p-2 w-[100px]"
     >
       <div className="flex items-center gap-1 group">
-        {getShorthandLanguageName(languages, i18n.language)}
-        {showArrowIcon(showLanguageOptions)}
+        <span> {language.charAt(0).toUpperCase() + language.slice(1, 3)}</span>
+        <div className="group-hover:text-slate-400">
+          {showLanguageOptions ? <IoIosArrowUp /> : <IoIosArrowDown />}
+        </div>
       </div>
-      {showListOfLanguages(showLanguageOptions)}
+      {showLanguageOptions && (
+        <ul className="absolute top-[100%] flex flex-col gap-4 w-full">
+          {languages.map((languageInfo) => (
+            <li
+              className={`${
+                languageInfo.code === i18n.language ? "text-slate-400" : ""
+              }`}
+              key={languageInfo.code}
+              onClick={() => i18n.changeLanguage(languageInfo.code)}
+            >
+              {languageInfo.language}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
