9f67a999cd64c5eee21e3f53db4db00010740563
alisher <areshil91@gmail.com>
2024-03-06 16:37:54 +0600

    refactorin in LanguageSelector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -3,16 +3,17 @@ import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import { useTranslation } from "react-i18next";
 
+const languages = [
+  { code: "en", language: "English" },
+  { code: "ru", language: "Русский" },
+];
+
 const LanguageSelector = () => {
   const [showLanguageOptions, setShowLanguageOptions] = useState(false);
   const { i18n } = useTranslation();
-  const languages = [
-    { code: "en", language: "English" },
-    { code: "ru", language: "Русский" },
-  ];
 
-  const language = languages.filter((lan) => lan.code === i18n.language)[0]
-    .language;
+  const foundLanguage = languages.find((lan) => lan.code === i18n.language);
+  const language = foundLanguage ? foundLanguage.language : "";
 
   return (
     <div
@@ -20,7 +21,12 @@ const LanguageSelector = () => {
       className="relative  gap-1 cursor-pointer  text-xl sm:text-2xl bg-slate-100  p-2 w-[100px]"
     >
       <div className="flex items-center gap-1 group">
-        <span> {language.charAt(0).toUpperCase() + language.slice(1, 3)}</span>
+        <span>
+          {" "}
+          {language
+            ? language.charAt(0).toUpperCase() + language.slice(1, 3)
+            : ""}
+        </span>
         <div className="group-hover:text-slate-400">
           {showLanguageOptions ? <IoIosArrowUp /> : <IoIosArrowDown />}
         </div>
